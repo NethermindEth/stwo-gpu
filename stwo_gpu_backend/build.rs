9a3a900e25da3101a886ee23b62436626e52a6c5
Tom Grosso <tgrosso@eryx.co>
2024-07-29 14:57:21 +0000

    Bugfix accumulationops
    It uses m31 arithmetic

--- stwo_gpu_backend/build.rs
+++ stwo_gpu_backend/build.rs
@@ -1,12 +1,12 @@
 use cmake;
 
 fn main() {
+    // Build cuda code
     println!("cargo:rerun-if-changed=../cuda/CMakeLists.txt");
     println!("cargo:rerun-if-changed=../cuda/src");
     println!("cargo:rerun-if-changed=../cuda/include");
 
     let destination = cmake::build("../cuda/");
 
-    // Build cuda code
     println!("cargo:rustc-link-search={}/lib/", destination.display());
 }
