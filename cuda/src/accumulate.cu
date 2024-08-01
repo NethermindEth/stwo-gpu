5d8ce15b9524fcafd62d03b0555e0f42ce6814cc
Tom Grosso <tgrosso@eryx.co>
2024-08-01 14:24:45 +0000

    WIP

--- cuda/src/accumulate.cu
+++ cuda/src/accumulate.cu
@@ -1,8 +1,6 @@
 #include "../include/accumulate.cuh"
 
-__global__ void
-accumulate_kernel(uint32_t *left_columns[4],
-                  uint32_t *right_columns[4]) {
+__global__ void accumulate_kernel(uint32_t *left_columns[4], uint32_t *right_columns[4]) {
     uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
     left_columns[0][i] = add(left_columns[0][i], right_columns[0][i]);
     left_columns[1][i] = add(left_columns[1][i], right_columns[1][i]);
