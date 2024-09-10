168d3defe04e56392feb63088ffdfb63f1b98762
Sergio Chouhy <sergio.chouhy@gmail.com>
2024-09-10 12:42:51 -0300

    minor change

--- cuda/src/gkr.cu
+++ cuda/src/gkr.cu
@@ -1,15 +1,16 @@
 #include "../include/utils.cuh"
 
-__global__ void gen_eq_evals_kernel(qm31 v, qm31 *y, uint32_t y_size, qm31 *evals) {
+__global__ void gen_eq_evals_kernel(qm31 v, qm31 *factors, uint32_t y_size, qm31 *evals) {
+    // Assumes `factors` holds 1 - y_i at position 2 * i and y_i at position 2 * i + 1
+    // for all i = 0, .., y_size - 1.
     // TODO: See if shared memory speeds this up
 
     unsigned int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
 
     qm31 eq_eval = v;
     unsigned int shifted_thread_index = thread_index;
-    for (int i = y_size - 1; i >= 0; i--) {
-        int offset = y_size * (shifted_thread_index & 1);
-        eq_eval = mul(eq_eval, y[i + offset]);
+    for (int i = 2 * y_size - 2; i >= 0; i -= 2) {
+        eq_eval = mul(eq_eval, factors[i + (shifted_thread_index & 1)]);
         shifted_thread_index >>= 1;
     }
     evals[thread_index] = eq_eval;
@@ -21,18 +22,18 @@ void gen_eq_evals(qm31 v, qm31 *y, uint32_t y_size, qm31 *evals, uint32_t evals_
     const unsigned int NUMBER_OF_BLOCKS = (evals_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
     int precomputed_table_byte_length = sizeof(qm31) * y_size * 2;
-    qm31 *precomputed_table = (qm31*)malloc(precomputed_table_byte_length);
+    qm31 *factors = (qm31*)malloc(precomputed_table_byte_length);
     for(int i = 0; i < y_size; i++) {
-        precomputed_table[i] = sub(m31{1}, y[i]);
-        precomputed_table[i + y_size] = y[i];
+        factors[2 * i] = sub(m31{1}, y[i]);
+        factors[2 * i + 1] = y[i];
     }
 
-    qm31 *precomputed_table_device;
-    cudaMalloc((void**)&precomputed_table_device, precomputed_table_byte_length);
-    cudaMemcpy(precomputed_table_device, precomputed_table, precomputed_table_byte_length, cudaMemcpyHostToDevice);
-    free(precomputed_table);
+    qm31 *factors_device;
+    cudaMalloc((void**)&factors_device, precomputed_table_byte_length);
+    cudaMemcpy(factors_device, factors, precomputed_table_byte_length, cudaMemcpyHostToDevice);
+    free(factors);
 
-    gen_eq_evals_kernel<<<NUMBER_OF_BLOCKS, min(evals_size, BLOCK_SIZE)>>>(v, precomputed_table_device, y_size, evals);
+    gen_eq_evals_kernel<<<NUMBER_OF_BLOCKS, min(evals_size, BLOCK_SIZE)>>>(v, factors_device, y_size, evals);
     cudaDeviceSynchronize();
-    cudaFree(precomputed_table_device);
+    cudaFree(factors_device);
 }
